f600bc4af15d7ea4ca7111490b71139af76e96c7
Vel <meisterrabyte@gmail.com>
2025-05-17 22:22:08 +0800

    Add validation check if valid numeric

--- main.cpp
+++ main.cpp
@@ -1,140 +1,142 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 using namespace std;
 
+vector<vector<string>> algorithms = {
+    {"Binary Search", "Interpolation Search", "Linear Search"},
+    {"Bubble Sort", "Insertion Sort", "Selection Sort"}
+};
+
+void printHeader(const string &heading){
+    string divider(70, '*');
+    cout << divider << "\n\t\t\t" << heading << "\n" << divider << endl;
+}
+
 void showAlgMenu(int op){
-    vector<vector<string>> algorithms = {{"Binary Search", "Interpolation Search", "Linear Search"},
-                                         {"Bubble Sort", "Insertion Sort", "Selection Sort"}};
-
-    op == 1 ? cout << "Select a searching algorithm:" : cout << "Select a sorting algorithm:";
-    cout << "\n";
-
+    printHeader(op == 1 ? "Searching Algorithms" : "Sorting Algorithms");
+    cout << "How would you like to sort?\n";
     for (int i = 0; i < algorithms[op - 1].size(); i++){
-        cout << "\t [" << i + 1 << "] " << algorithms[op - 1][i] << endl;
+    	cout << "\t [" << i + 1 << "] " << algorithms[op - 1][i] << endl;
+	}
+}
+
+bool isValidNum(int &num, int lower = INT_MIN, int upper = INT_MAX){
+    cin >> num;
+    if (cin.fail() || num < lower || num > upper){
+        cout << "Invalid input!\n\n";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return false;
     }
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    return true;
 }
 
 class ProblemSet{
 protected:
     vector<int> nums;
-
-    ProblemSet(){
-        int sizeArr;
+    bool isValidSet = true;
+    
+    ProblemSet(int op, int algChoice){
+        printHeader(algorithms[op - 1][algChoice - 1]);
         cout << "Enter number of elements: ";
-        cin >> sizeArr;
+        int sizeArr;
+        
+        if (!isValidNum(sizeArr, 1)){isValidSet = false; return;}
 
         cout << "\n";
-
         for (int i = 0; i < sizeArr; i++){
             int num;
             cout << "Element " << i + 1 << ": ";
-            cin >> num;
+            if (!isValidNum(num)){isValidSet = false; return;}
             nums.push_back(num);
         }
     }
 };
 
 class SortProblem : public ProblemSet{
-private:
-    void swap(int &num1, int &num2){
-        int temp = num1;
-        num1 = num2;
-        num2 = temp;
-    }
-
 protected:
     int algChoice;
+
     void bubSort(){
-        for(int i = 0; i < nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++){
             bool unchanged = true;
-            
-            for(int j = 0; j < nums.size() - i - 1; j++){
-                if(nums[j + 1] < nums[j]){
+            for (int j = 0; j < nums.size() - i - 1; j++){
+                if (nums[j + 1] < nums[j]){
                     swap(nums[j], nums[j + 1]);
                     unchanged = false;
                 }
             }
-            
             if (unchanged) {break;}
-        }
-    }
-
-    void insSort(){
-        for (int i = 1; i < nums.size(); i++){
-            for (int j = i; j > 0; j--){
-                if (nums[j - 1] > nums[j]){
-                    swap(nums[j], nums[j - 1]);
-                }
-            }
         }
     }
 
     void selSort(){
         for (int i = 0; i < nums.size(); i++){
             int indexMin = i;
-            
-			for (int j = i + 1; j < nums.size(); j++){
-                if (nums[j] < nums[indexMin]){
-                    indexMin = j;
-                }
+            for (int j = i + 1; j < nums.size(); j++){
+                if (nums[j] < nums[indexMin]) {indexMin = j;}
             }
-            
-            if (indexMin == i){continue;}
             swap(nums[i], nums[indexMin]);
         }
     }
 
 public:
-    SortProblem(int algChoice) : ProblemSet(), algChoice(algChoice) {}
+    SortProblem(int op, int choice) : ProblemSet(op, choice), algChoice(choice) {}
+
     void arrange(){
+        if (!isValidSet){return;}
+
         switch (algChoice){
 	        case 1: bubSort(); break;
 	        case 2: insSort(); break;
 	        case 3: selSort(); break;
-	        default:
-	            cout << "Invalid choice!";
-	            break;
+	        default: cout << "Invalid choice!"; return;
         }
 
         cout << "Sorted Array: ";
-        for(int num : nums){ cout << num << " "; }
+        for (int num : nums){cout << num << " ";}
         cout << endl;
+    }
+
+    void insSort(){
+        for (int i = 1; i < nums.size(); i++){
+            for (int j = i; j > 0 && nums[j - 1] > nums[j]; j--){
+            	swap(nums[j], nums[j - 1]);
+			}
+        }
     }
 };
 
 class SearchProblem : public SortProblem{
-private:
     int target;
 
     void binSearch(){
-        int low = 0;
-        int high = nums.size() - 1;
-
+        int low = 0, high = nums.size() - 1;
         while (low <= high){
             int mid = (low + high) / 2;
-
             if (nums[mid] == target){
                 cout << "Element found at index: " << mid << endl;
                 return;
             }
-
-			nums[mid] > target ? high = mid - 1 : low = mid + 1;
+            nums[mid] > target ? high = mid - 1 : low = mid + 1;
         }
         cout << "Element not found." << endl;
     }
 
     void interpolSearch(){
-        int low = 0;
-        int high = nums.size() - 1;
-
+        int low = 0, high = nums.size() - 1;
         while (low <= high && target >= nums[low] && target <= nums[high]){
+            if (nums[high] == nums[low]) {break;}
+
             int mid = low + ((high - low) * (target - nums[low])) / (nums[high] - nums[low]);
 
             if (nums[mid] == target){
                 cout << "Element found at index: " << mid << endl;
                 return;
             }
-
+            
             nums[mid] > target ? high = mid - 1 : low = mid + 1;
         }
         cout << "Element not found." << endl;
@@ -142,7 +144,7 @@ private:
 
     void linSearch(){
         for (int i = 0; i < nums.size(); i++){
-            if (target == nums[i]){
+            if (nums[i] == target){
                 cout << "Element found at index: " << i << endl;
                 return;
             }
@@ -151,58 +153,55 @@ private:
     }
 
 public:
-    SearchProblem(int algChoice) : SortProblem(algChoice){
+    SearchProblem(int op, int choice) : SortProblem(op, choice){
+        if (!isValidSet) {return;}
+
         cout << "Enter value to search: ";
-        cin >> target;
+        if (!isValidNum(target)){isValidSet = false;}
     }
 
     void search(){
-        this->insSort();
-        
-		switch (algChoice){
+        if (!isValidSet){return;}
+
+        insSort();
+
+        switch (algChoice){
 	        case 1: binSearch(); break;
 	        case 2: interpolSearch(); break;
 	        case 3: linSearch(); break;
-	        default:
-	            cout << "Invalid choice!";
-	            break;
+	        default: cout << "Invalid choice!"; break;
         }
     }
 };
 
 int main(){
     while (true){
-        int op;
-        cout << "Select an option:" << endl;
+        printHeader("Search & Sort Program");
+        
+        cout << "What operation would you like to do?" << endl;
         cout << "\t" << "[1] Searching Algorithm" << endl;
         cout << "\t" << "[2] Sorting Algorithm" << endl;
         cout << "\t" << "[3] Exit" << endl;
         cout << "Enter option here: ";
-        cin >> op;
+        
+        int op;
+        if (!isValidNum(op, 1, 3)){continue;}
 
-        if (op == 3){break;}
+        if (op == 3){
+            cout << "\n" <<"Thank you for using the Search & Sort Program";
+            break;
+        }
 
         cout << "\n";
-
+        
         showAlgMenu(op);
         int alg;
         cout << "Enter option here: ";
-        cin >> alg;
-
+        
+        if (!isValidNum(alg, 1, 3)){continue;}
         cout << "\n";
 
-        if (op == 1){
-            SearchProblem s = SearchProblem(alg);
-            s.search();
-        }
-        else if (op == 2){
-            SortProblem p = SortProblem(alg);
-            p.arrange();
-        }
-        else
-        {
-            cout << "Invalid option!" << endl;
-        }
+        op == 1 ? SearchProblem(op, alg).search() : SortProblem(op, alg).arrange();
     }
     return 0;
 }
